c0a324760a25714233d97d1023bbbd9cc96ca4c7
anuragrai63 <66570766+anuragrai63@users.noreply.github.com>
2024-06-01 23:09:15 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider aws {
+    region="us-east-1"
+    }
+
+resource "aws_instance" "vs-test" {
+    ami = "ami-07caf09b362be10b8"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-0d0e3828e26eb661c"
+    availability_zone = "us-east-1d"
+    tags = {
+      "Name" = "vs-1stinstance"
+    }
+  
+}
+
