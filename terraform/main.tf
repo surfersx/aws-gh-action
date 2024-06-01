fe84585452035238ac30f247b3b7697ea9446415
anuragrai63 <66570766+anuragrai63@users.noreply.github.com>
2024-06-01 23:31:42 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "vs-test" {
     subnet_id = "subnet-0d0e3828e26eb661c"
     availability_zone = "us-east-1d"
     tags = {
-      "Name" = "VSS-1stinstance"
+      "Name" = "VS-1stinstance"
     }
   
 }
