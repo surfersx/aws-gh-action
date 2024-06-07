a1498c65fb5dcc6a7ccc4ac2624602a2d81f7c30
anuragrai63 <66570766+anuragrai63@users.noreply.github.com>
2024-06-07 23:54:40 +0530

    Update main.tf (#11)

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "aws_ssm_parameter" "foo" {
 }
 
 resource "aws_instance" "vs-test" {
-    ami = "ami-07caf09b362be10b8"
+    ami = "ami-00beae93a2d981137"
     instance_type = "t2.micro"
     subnet_id = "subnet-0d0e3828e26eb661c"
     availability_zone = "us-east-1d"
