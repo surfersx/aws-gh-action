825e035fad783bcf060d898d816bb9573d2ee85f
anuragrai63 <66570766+anuragrai63@users.noreply.github.com>
2024-06-16 17:21:03 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,10 @@ resource "aws_ssm_parameter" "foo" {
   value = "barr"
 }
 
+variable "test" {
+  type = string
+  }
+
 resource "aws_instance" "vs-test" {
     ami = "ami-00beae93a2d981137"
     instance_type = "t2.micro"
